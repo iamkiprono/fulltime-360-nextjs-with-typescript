c839024fe4c2fdb5a610575e30c689b1b3ed64a7
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-11-11 10:51:03 +0300

    Update Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,28 +1,38 @@
+"use client"
+
+
 import MyCard from "@/components/MyLibrary/MyCard";
 import { Blogs } from "@/types/types";
 import React from "react";
 import Visit from "./Visit";
+import { useState, useEffect } from "react";
 
-const getBlogs = async (): Promise<Blogs[]> => {
-  const res = await fetch("https://blog-api-kiprono.onrender.com/blogs", {
-    next: {
-      revalidate: 0, //opt out of caching
-      tags: ["blogs"],
-    },
-  });
-  const data = await res.json();
-  return data;
-};
+const Homepage = () => {
+  const [blogs, setBlogs] = useState<Blogs[] | null>(null);
 
-const Homepage = async () => {
-  const blogs = await getBlogs();
+  const getBlogs = async () => {
+    const res = await fetch("https://blog-api-kiprono.onrender.com/blogs", {
+      next: {
+        revalidate: 0, //opt out of caching
+        tags: ["blogs"],
+      },
+    });
+    const data = await res.json();
 
+    setBlogs(data);
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
   return (
     <div>
       <div className="flex flex-wrap gap-6 justify-center  mx-auto">
-        {blogs.map((blog) => {
-          return <MyCard key={blog._id} blogs={blog} />;
-        })}
+        {!blogs
+          ? "Loading..."
+          : blogs?.map((blog) => {
+              return <MyCard key={blog._id} blogs={blog} />;
+            })}
         <Visit />
       </div>
     </div>
